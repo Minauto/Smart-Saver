4aacd4cd09d377692d7f850934328e8c83eab6c7
viliusan <vilius.andraitis@mif.stud.vu.lt>
2020-12-28 17:53:51 +0200

    Front-end design stuff

--- smartsaver.frontend/src/App.js
+++ smartsaver.frontend/src/App.js
@@ -1,14 +1,30 @@
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
+import { Navbar } from 'react-bootstrap';
+import Logo from './save-money.png';
 
 const App = () => (
-  <div style={{ width: '60%', margin: 'auto' }}>
-    <h3>New Expense</h3>
-    <ExpenseForm />
-    <hr style={{ border: '1px solid grey'}} />
-    <h3> Your Expenses</h3>
-    <ExpenseList />
-  </div>
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand style={{ fontFamily: 'Raleway' }}>
+        <img
+          alt=""
+          src={Logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      Smart Saver
+    </Navbar.Brand>
+    </Navbar>
+    <div style={{ width: '60%', margin: 'auto', marginTop: '20px' }}>
+      <h3>New Expense</h3>
+      <ExpenseForm />
+      <hr style={{ border: '1px solid grey' }} />
+      <h3> Your Expenses</h3>
+      <ExpenseList />
+    </div>
+  </>
 );
 
 export default App;
