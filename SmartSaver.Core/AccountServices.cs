ad802c5ea3ca767582dc1fab29ed20c8c4196d47
Mindaugas Zabotka <mzabotka@gmail.com>
2021-01-07 18:35:08 +0200

    GET/CREATE created for accounts

--- SmartSaver.Core/AccountServices.cs
+++ SmartSaver.Core/AccountServices.cs
@@ -1,21 +1,77 @@
-﻿using SmartSaver.DB;
+﻿using Microsoft.Data.SqlClient;
+using SmartSaver.DB;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace SmartSaver.Core
 {
     public class AccountServices : IAccountServices
     {
+
+        SqlConnection con = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Database=SmartSaverDB;Integrated Security=True");
         public Account GetAccount(string nickName)
         {
             Account account = new Account();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * From Accounts WHERE Nickname = '" + nickName + "'", con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
 
-            return account;
+                String nameRead = reader["Nickname"].ToString();
+                int IdRead = Int32.Parse(reader["UserId"].ToString());
+
+                reader.Close();
+                con.Close();
+
+                account.Nickname = nameRead;
+                account.UserId = IdRead;
+
+                return account;
+            }
+            catch (Exception exc)
+            {
+                con.Close();
+                return account;
+            }
         }
+
+        
         public bool CreateAccount(Account account)
         {
-            return true;
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Accounts where Nickname='" + account.Nickname + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "0")
+            {
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+
+                cmd.CommandText = "INSERT Accounts (Nickname, Password) VALUES ('" + account.Nickname + "', '" + account.Password + "')";
+                cmd.Connection = con;
+
+                con.Open();
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception exc)
+                {
+                    return false;
+                }
+                con.Close();
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
         }
         public bool DeleteAccount(string nickName)
         {
